07e3e4c93fbf148ee584f01694ad078f9b854798
imnotseanwtf <seangulliverdoctora@gmail.com>
2025-02-20 19:36:34 +0800

    background img login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,8 @@
 
 <body>
     <main>
-        <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
+        <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center"
+            style="background-image: url({{ asset('images/login/background-login.jpg') }}); background-size: cover; background-position: center;">
             @yield('content')
         </section>
     </main>
